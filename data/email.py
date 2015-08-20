11e92d50d77ad79cf90a863cb006732abad72bfd
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-20 14:41:52 +0530

    docs

--- data/email.py
+++ data/email.py
@@ -1,5 +1,4 @@
 import sendgrid
-from data import load_json
 from . import sendgrid_conf
 
 if not sendgrid_conf:
@@ -9,12 +8,19 @@ if not sendgrid_conf:
     }
 
 
-_sclient = sendgrid.SendGridClient(username_or_apikey=sendgrid_conf['username'], password=sendgrid_conf['password'])
+_sclient = sendgrid.SendGridClient(
+    username_or_apikey=sendgrid_conf['username'],
+    password=sendgrid_conf['password'])
 
 def send_mail(email_id, subject, body):
+    """ Send the actual email
+    :param email_id: valid email id
+    :param subject: String
+    :param body: Html string
+    """
     email = sendgrid.Mail()
     email.add_to(email_id)
     email.set_subject(subject)
     email.set_html(body)
-    email.set_from('<no-reply @ wadi.com>')
+    email.set_from("<no-reply @ wadi.com>")
     return _sclient.send(email)
