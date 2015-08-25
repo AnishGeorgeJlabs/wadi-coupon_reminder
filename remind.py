a934acdc58ff31cc5f868185f4d62a42a1bbc19c
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-25 14:13:22 +0530

    Wrote test for main

--- remind.py
+++ remind.py
@@ -3,5 +3,41 @@ Main Module for the coupon reminder system
 """
 
 from jinja2 import Environment, PackageLoader
+from data.email import send_mail
+from datetime import datetime, timedelta
 
 env = Environment(loader=PackageLoader('data', 'templates'))
+template = env.get_template('email.html')
+
+# Some external code from stack overflow
+def ord(n):
+    return str(n) + ("th" if 4 <= n % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th"))
+
+def dtStylish(dt, f):
+    return dt.strftime(f).replace("{th}", ord(dt.day))
+# --------------------------------------
+
+
+
+def run(data, target_date):
+    dt = dtStylish(target_date, "{th} %B, %Y")
+    for email_id, coupons in data.items():
+        content = template.render(coupons=coupons, target=dt)
+        send_mail(email_id=email_id, subject="Wadi coupon test", body=content)
+
+
+def test():
+    data = {
+        'anish.george@jlabs.co': [
+            {
+                'code': 'WELCOMEwadi',
+                'amount': '400 AED',
+                'total': '100 AED'
+            }
+        ]
+    }
+    run(data, datetime.now() + timedelta(days=7))
+
+
+if __name__ == '__main__':
+    pass
